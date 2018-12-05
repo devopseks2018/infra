194770dd52828bb7c21038b1e3d9273dfd506dac
devopseks2018 <devopseks@gmail.com>
2018-12-05 23:51:32 +0100

    commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 # Modify this file to make it your own :-)
 
 variable "app_prefix" {
-  default = "pgr301-zzz"
+  default = "devops-pipeline"
 }
 
 variable "pipeline_name" {
-  default = "pgr301-your-initialsz"
+  default = "exam-pipeline"
 }
